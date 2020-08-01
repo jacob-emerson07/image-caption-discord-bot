dffbe925638bb3d8d7d9e51123aff845699463b7
zach dorame barajas <zachdoramebarajas@gmail.com>
2020-08-01 15:16:02 -0700

    Formerly did not accept discord image urls as images, now accepts them

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 #TODO:
-# -make it work for all images and not just URL images
+# -make bot detect poor urls
+
+def validEnd(imageExt):
+    img_type = ['jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG']
+    for extension in img_type:
+        if imageExt.endswith(extension):
+            return True
+
 
 @client.event
 async def on_message(message):
@@ -15,10 +22,18 @@ async def on_message(message):
         return
 
     try:
-        imgUrl = message.attachments[0].url
-        print(imgUrl)
-        await message.channel.send(imgUrl)
+        img_url = message.attachments[0].url
+        print(img_url)
+        await message.channel.send(img_url)
     except IndexError:
-        print("Image not detected")
+        try:
+            img_url = message.content
+            if img_url.startswith("https://cdn.discordapp.com/attachments/") and validEnd(img_url):
+                print(img_url)
+                await message.channel.send(img_url)
+            else:
+                raise IndexError
+        except IndexError:
+            print("Image not detected")
 
-client.run('~~~')
+client.run('NzM5MjI0NjA2NDQzMDQ0ODc4.XyXWkA.w6YH6c5M-FeaS82LGUfL_XBNDyQ')
